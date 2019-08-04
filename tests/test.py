744ebe62e82cf4eed10be42a068c4780347922e5
Christopher Torgalson <manager@bedlamhotel.com>
2019-08-04 14:01:40 +0200

    Adds tests setup, basic test for preference contents

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+
+import testinfra.utils.ansible_runner
+
+
+@pytest.mark.parametrize("domain,string", [
+    ("com.googlecode.iterm2", "Name = Dev"),
+])
+def test_preference_contents(host, domain, string):
+    contents = host.check_output("defaults read {} | grep ''".format(domain))
+
+    assert string in contents
